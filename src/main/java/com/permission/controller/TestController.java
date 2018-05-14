286b857231fd53d76a1e82e743e8dca42af3cd96
zt19994 <zt191610942@163.com>
2018-05-14 21:04:49 +0800

    config

---
+++ src/main/java/com/permission/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.permission.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * @author zt1994 2018/5/9 21:03
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        logger.info("hello");
+        return "hello, permission";
+    }
+}
---
+++ src/main/java/com/permission/dao/TestDao.java
@@ -0,0 +1,7 @@
+package com.permission.dao;
+
+/**
+ * @author zt1994 2018/5/14 20:44
+ */
+public interface TestDao {
+}
