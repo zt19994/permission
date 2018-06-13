4ccc6ea8140bc0be24d7467bb198a364b34f5430
zt19994 <zt191610942@163.com>
2018-06-13 20:46:58 +0800

    add Interceptor

---
+++ src/main/java/com/permission/common/HttpInterceptor.java
@@ -0,0 +1,48 @@
+package com.permission.common;
+
+import com.permission.util.JsonMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * @author zt1994 2018/6/12 21:00
+ */
+public class HttpInterceptor extends HandlerInterceptorAdapter{
+
+    private static final Logger logger = LoggerFactory.getLogger("HttpInterceptor");
+
+    private static final String  START_TIME = "requestStartTime";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI().toString();
+        Map parameterMap = request.getParameterMap();
+        logger.info("request start: url:{}, params:{}", url, JsonMapper.obj2String(parameterMap) );
+        long start = System.currentTimeMillis();
+        request.setAttribute(START_TIME, start);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String url = request.getRequestURI().toString();
+        long start = (long) request.getAttribute(START_TIME);
+        long end = System.currentTimeMillis();
+        logger.info("request finished: url:{}, cost:{}",url,  end - start );
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String url = request.getRequestURI().toString();
+        long start = (long) request.getAttribute(START_TIME);
+        long end = System.currentTimeMillis();
+        logger.info("request completion: url:{}, cost:{}",url,  end - start );
+    }
+}
